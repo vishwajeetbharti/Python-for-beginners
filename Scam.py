ec428d19f4c2fcea8c68656f460dbd3ff1a8cf77
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2022-10-12 00:46:15 +0530

    Type massege and enter  code

---
+++ Scam.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+time.sleep(1)
+count=0
+while count<=1000:
+    pyautogui.typewrite("Gandu broker, Kashak weds Chandan gandu ")
+    pyautogui.press("enter")
+    count= count+1
